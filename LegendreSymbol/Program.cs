f743e11f06a11d8ca010984513ff6129f7528be2
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2019-05-10 12:07:55 +0500

    Completed

--- LegendreSymbol/Program.cs
+++ LegendreSymbol/Program.cs
@@ -1,64 +1,80 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace LegendreSymbol
 {
     class Program
     {
+        const int AmountOfLegendreSymbols = 240;
+        const int TextLength = AmountOfLegendreSymbols / 8;
+
         static void Main(string[] args)
         {
+            StringBuilder sb = new StringBuilder(TextLength);
             Regex r = new Regex(@"L\(([0-9]+),([0-9]+)\)");
 
+            using (StreamWriter sw = new StreamWriter("legendre.txt", false))
             using (StreamReader sr = new StreamReader(args[0]))
             {
                 string line;
+                int bit = 1;
+                int symbol = 0;
+                int legendreSymbol = 0;
 
-                int i = 0;
-                while((line = sr.ReadLine()) != null)
+                sw.WriteLine("Legendre symbols");
+                while ((line = sr.ReadLine()) != null)
                 {
                     Match m = r.Match(line);
 
-                    if(m.Success)
+                    if (m.Success)
                     {
-                        Console.WriteLine(Legendre(long.Parse(m.Groups[1].Value), long.Parse(m.Groups[2].Value)));
-                        i++;
+                        legendreSymbol = Legendre(long.Parse(m.Groups[1].Value), long.Parse(m.Groups[2].Value));
+                        sw.Write(legendreSymbol + ",");
+                        if (bit > 128)
+                        {
+                            bit = 1;
+                            if (symbol != 32)
+                                symbol += 848;
+                            sb.Append((char)symbol);
+                            symbol = 0;
+                        }
+                        symbol += legendreSymbol * bit;
+                        bit <<= 1;
                     }
-                    if (i > 10) break;
                 }
+                if (bit > 128)
+                {
+                    if (symbol != 32)
+                        symbol += 848;
+                    sb.Append((char)symbol);
+                }
+                sw.WriteLine("\n\nText");
+                sw.Write(sb.ToString());
             }
         }
 
-        static long Legendre(long a, long p)
+        static int Legendre(long a, long p)
         {
-            if (p < 2)  // prime test is expensive.
-                throw new ArgumentOutOfRangeException("p", "p must not be < 2");
-            if (a == 0)
+            if (a % p == 0) return 0;
+            return ModularPower(a, (p - 1) / 2, p) == 1 ? 1 : 0;
+        }
+
+        static long ModularPower(long a, long p, long n)
+        {
+            long res = 1;
+
+            a %= n;
+
+            while (p > 0)
             {
-                return 0;
+                if ((p & 1) == 1)
+                    res = (res * a) % n;
+
+                p >>= 1;
+                a = (a * a) % n;
             }
-            if (a == 1)
-            {
-                return 1;
-            }
-            long result;
-            if (a % 2 == 0)
-            {
-                result = Legendre(a / 2, p);
-                if (((p * p - 1) / 8) != 0) // instead of dividing by 8, shift the mask bit
-                {
-                    result = -result;
-                }
-            }
-            else
-            {
-                result = Legendre(p % a, a);
-                if (((a - 1) * (p - 1) / 4) != 0) // instead of dividing by 4, shift the mask bit
-                {
-                    result = -result;
-                }
-            }
-            return result;
+            return res;
         }
     }
 }
