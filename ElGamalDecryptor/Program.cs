4d4c13daf695fe754a0863ec109eb3366edc5bd3
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2019-05-10 19:47:59 +0500

    Initial commit

---
+++ ElGamalDecryptor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ElGamalDecryptor
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex("[0-9]+");
+
+            using (StreamReader sr = new StreamReader(args[0]))
+            {
+                long p = long.Parse(regex.Match(sr.ReadLine()).Value);
+                long q = long.Parse(regex.Match(sr.ReadLine()).Value);
+                long y = long.Parse(regex.Match(sr.ReadLine()).Value);
+                sr.ReadLine();
+
+                
+            }
+        }
+    }
+}
