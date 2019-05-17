4bc1372acac09a4cae2c241c3faec3d5c48849a1
Vladimir Matushkin <matushkin.vladimir98@gmail.com>
2019-05-17 12:12:06 +0500

    Reworked, fix some bugs

--- Steganography/Program.cs
+++ Steganography/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Steganography
 {
@@ -20,44 +21,50 @@ namespace Steganography
             process.StartInfo.RedirectStandardOutput = true;
             //process.StartInfo.RedirectStandardError = true;
 
-            DirectoryInfo directoryInfo = new DirectoryInfo(PathToImages);
-            FileInfo[] images = directoryInfo.GetFiles("*.jpg", SearchOption.TopDirectoryOnly);
-
-            Regex regexEmbedded = new Regex("embedded", RegexOptions.RightToLeft | RegexOptions.Compiled);
-            Regex regexBits = new Regex("[0-9]+", RegexOptions.Compiled);
+            Regex regexEmbedded = new Regex(@"([0-9]+) bits \([0-9]+ bytes\) embedded", RegexOptions.RightToLeft | RegexOptions.Compiled);
             Regex regexResolution = new Regex("[0-9]+ x [0-9]+", RegexOptions.Compiled);
 
-            using (StreamWriter sw = new StreamWriter("result.csv", false, Encoding.UTF8))
+            DirectoryInfo directoryInfo = new DirectoryInfo(PathToImages);
+            FileInfo[] images = directoryInfo.GetFiles("*", SearchOption.TopDirectoryOnly);
+            
+            Console.Write("Specify delimiter in csv file: ");
+            string delimiter = Char.ConvertFromUtf32(Console.Read());
+            string header = string.Join(delimiter, "File name", "Resolution", "Size of embeddeded information(bits)",
+                                        "Size of base image(bits)", "% of embeddeded information");
+
+            using (FileSystemWatcher watcher = new FileSystemWatcher(PathToImages, "*.jpg"))
+            using(StreamWriter sw = new StreamWriter("result.csv", false, Encoding.UTF8))
             {
-                sw.WriteLineAsync("File name,Resolution,Size of embeddeded information(bits),Size of base image(bits),% of embeddeded information");
+                sw.WriteLineAsync(header);
+                //watcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite;
 
                 foreach (FileInfo image in images)
                 {
                     process.StartInfo.Arguments = Arguments + image.Name;
                     process.Start();
 
-                    string resolution = null;
-                    ulong embeddedBits = 0;
-                    while (process.StandardOutput.EndOfStream == false)
-                    {
-                        string output = process.StandardOutput.ReadLine();
-                        Console.WriteLine(output);
-                        if (resolution == null && regexResolution.IsMatch(output))
-                        {
-                            resolution = regexResolution.Match(output).Value;
-                        }
-                        else if (regexEmbedded.IsMatch(output))
-                        {
-                            embeddedBits = ulong.Parse(regexBits.Match(output).Value);
-                            Console.WriteLine(process.StandardOutput.ReadToEnd());
-                            break;
-                        }
-                    }
-                    //string err = process.StandardError.ReadToEnd();
+                    Task<string> readOutput = process.StandardOutput.ReadToEndAsync();
+
+                    WaitForChangedResult result = watcher.WaitForChanged(WatcherChangeTypes.Created);
+                    string pathToImage = PathToImages + result.Name;
+
+                    readOutput.Wait();
+                    string output = readOutput.Result;
+
+                    string resolution = regexResolution.Match(output).Value;
+                    ulong embeddedBits = ulong.Parse(regexEmbedded.Match(output).Groups[1].Value);
+
                     process.WaitForExit();
-                    sw.WriteLineAsync($"{image.Name},{resolution},{embeddedBits},{image.Length * 8},"
-                                      + $"{embeddedBits * 100 / (double)(image.Length * 8):0.00}");
-                    //process.Close();
+
+                    long length = new FileInfo(pathToImage).Length;
+
+                    string line = string.Join(delimiter, image.Name, resolution, embeddedBits, length * 8, 
+                            string.Format("{0:0.00}", (embeddedBits * 100) / (double)(length * 8)));
+                    sw.WriteLineAsync(line);
+
+                    Console.WriteLine(result.Name);
+
+                    File.Delete(pathToImage);
                 }
             }
         }
